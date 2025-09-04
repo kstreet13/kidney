1d2d4147e1b316bee17191310922c502c2a8a3db
Kelly Street <street.kelly@gmail.com>
2025-09-04 11:26:29 -0700

    Update labelTransfer.R

--- labelTransfer.R
+++ labelTransfer.R
@@ -1,6 +1,7 @@
 pca <- readRDS('~/Downloads/cheese_pca.rds')
 umap <- readRDS("~/Downloads/cheese_umap_annotated_v2.rds")
 clus2transfer <- umap$banana_celltype
+K <- 3
 # for clus2transfer, only the scRNAseq labels matter
 
 
@@ -10,8 +11,8 @@ assay[which(umap$og_celltype == 'roi1')] <- 'roi1'
 assay[which(umap$og_celltype == 'roi2')] <- 'roi2'
 
 # transfer labels for roi1,roi2
-# get KNN (k=3) for roi1,roi2 in scRNAseq
-# if 2 NNs in scRNAseq share label, transfer it
+# get KNN for roi1,roi2 in scRNAseq
+# transfer most common label (unless there is no plurality)
 
 require(BiocNeighbors)
 require(Matrix); require(matrixStats)
@@ -19,14 +20,15 @@ require(Matrix); require(matrixStats)
 # find nearest neighbors
 knn <- queryKNN(X = pca[which(assay=='RNA'), ],
                 query = pca[which(assay %in% c('roi1','roi2')), ],
-                k = 3, get.distance = FALSE)
+                k = K, get.distance = FALSE)
 # build vector of transfered labels
 tran <- apply(knn$index, 1, function(idx){
     nnLabels <- clus2transfer[idx]
-    if(any(duplicated(nnLabels))){
-        return(nnLabels[duplicated(nnLabels)][1])
-    }else{
+    tab <- sort(table(nnLabels),decreasing = TRUE)
+    if(tab[1] == tab[2]){
         return('unclear')
+    }else{
+        return(names(tab)[1])
     }
 })
 
